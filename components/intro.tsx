421129937fc441e0c825aa6ca079e045ef724171
sief aqel <sief.aqel@teklabz.com>
2025-09-28 13:03:14 +0300

    fixed intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import React from "react";
 import authorImage from "../public/images/authors/authorImage.jpeg";
+
 function Intro() {
   return (
-    <section className="flex-col-revers flex items-start gap-x-10 gap-y-4">
-      <div className="mt-2 flex-1 md:mt-0">
-        <span className="title">Hey, I&#39;m Saif.</span>
-        <p className="text-muted-foreground mt-3 text-sm font-light">
+    <section className="flex flex-col-reverse items-center gap-6 px-4 md:flex-row md:items-start md:gap-10 md:px-0">
+      <div className="flex-1">
+        <span className="text-3xl font-bold">Hey, I&apos;m Saif.</span>
+        <p className="text-muted-foreground mt-3 text-sm font-light md:text-base">
           Frontend-focused Software Developer with 2+ years’ experience building
           responsive web applications in React and TypeScript, plus hands-on
           exposure to full-stack (Node.js, Express, MongoDB). Experienced in
@@ -18,19 +19,21 @@ function Intro() {
         <a
           href="/Saif_Aqel_Resume.pdf"
           download
-          className="mt-4 inline-block rounded bg-[#ad9830] px-4 py-2 text-white hover:bg-[#927f28]"
+          className="mt-4 inline-block rounded bg-[#ad9830] px-4 py-2 font-medium text-white transition-colors duration-200 hover:bg-[#927f28]"
         >
           Download CV
         </a>
       </div>
-      <Image
-        alt="Saif Aqel Image"
-        src={authorImage}
-        className="rounded-lg"
-        width={250}
-        height={250}
-        priority
-      />
+
+      <div className="relative h-48 w-48 md:h-64 md:w-64">
+        <Image
+          alt="Saif Aqel Image"
+          src={authorImage}
+          className="rounded-lg object-cover"
+          fill
+          priority
+        />
+      </div>
     </section>
   );
 }
